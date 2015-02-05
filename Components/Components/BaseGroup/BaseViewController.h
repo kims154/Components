c00dc881d95617ee33e59f7b46d54e15e944b1f3
Damon <xdg_1988@qq.com>
2015-02-05 16:27:58 +0800

    添加baseViewController

---
+++ Components/Components/BaseGroup/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  Components
+//
+//  Created by Damon on 15/2/5.
+//  Copyright (c) 2015年 Damon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
